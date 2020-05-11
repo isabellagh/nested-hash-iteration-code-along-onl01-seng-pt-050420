5549cf44ab60fcc3be605932b5811454d60a5b2a
isabella g henriques <isabellagh88@gmail.com>
2020-05-11 14:43:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      
     end
   end
 end
